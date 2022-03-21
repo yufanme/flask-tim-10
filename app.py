ad4ca05c4a604f4a0f5741c482312fd2235e224a
yufanme <562937707@qq.com>
2022-03-21 17:08:12 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from admin.second import second
+
+app = Flask(__name__)
+app.register_blueprint(second, url_prefix="/admin")
+
+
+@app.route('/')
+def test():
+    return "<h1>Test</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
